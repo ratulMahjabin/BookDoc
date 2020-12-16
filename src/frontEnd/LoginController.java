8cc23876cb61f69192e3d196916228bd10cb931b
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2020-12-16 15:25:13 +0600

    Update to login frontend

--- src/frontEnd/LoginController.java
+++ src/frontEnd/LoginController.java
@@ -1,4 +1,17 @@
 package frontEnd;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class LoginController {
+    ObservableList<String>roleChoiceList = FXCollections.observableArrayList("Admin","Doctor","Receptionist");
+    @FXML
+    private ChoiceBox roleChoiceBox;
+    @FXML
+    private void initialize(){
+        roleChoiceBox.setValue("Admin");
+        roleChoiceBox.setItems(roleChoiceList);
+    }
 }
--- src/frontEnd/Main.java
+++ src/frontEnd/Main.java
@@ -1,5 +1,25 @@
 package frontEnd;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("UserLogin");
+        primaryStage.setScene(new Scene(root,600,400));
+        primaryStage.show();
+
+    }
 }
