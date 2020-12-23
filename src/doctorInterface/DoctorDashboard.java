6e7579f7bc7f0b76171469f3aa7290ffd5f131ae
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2020-12-23 16:07:16 +0600

    Update to Doctor Interface
    Dashboard, prescription frontend Design

---
+++ src/doctorInterface/DoctorDashboard.java
@@ -0,0 +1,18 @@
+package doctorInterface;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DoctorDashboard {
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnMakePrescription;
+
+    @FXML
+    private Button btnShowPrescription;
+
+
+
+}
---
+++ src/doctorInterface/MakePrescription.java
@@ -0,0 +1,4 @@
+package doctorInterface;
+
+public class MakePrescription {
+}
